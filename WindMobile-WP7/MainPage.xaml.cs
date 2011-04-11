01eae7fa19de69bdb56a77d632573c54ff87fc88
Cedric <ctt@veltigroup.com>
2011-04-11 15:58:56 +0200

    Create the project

---
+++ WindMobile-WP7/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace Ch.Epix.WindMobile.WP7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
