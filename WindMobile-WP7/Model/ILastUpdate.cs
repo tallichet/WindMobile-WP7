4e3c5df8afea96f19792d8f39f994afaab660971
Cedric <ctt@veltigroup.com>
2011-04-11 17:21:54 +0200

    Add the Model interfaces

---
+++ WindMobile-WP7/Model/ILastUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch.Epix.WindMobile.WP7.Model
+{
+    public interface ILastUpdate
+    {
+        DateTime Date {get;}
+    }
+}
---
+++ WindMobile-WP7/Model/IStationData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Ch.Epix.WindMobile.WP7.Model
+{
+    public interface IStationData
+    {
+        string StationId { get; }
+        MaintenanceStatus Status { get; }
+        DateTime ExpirationDate { get; }
+        DateTime LastUpdate { get; }
+        double WindAverage { get; }
+        double WindMax { get; }
+        int WindTrend { get; }
+        double WindHistoryMin { get; }
+        double WindHistoryAverage { get; }
+        double WindHistoryMax { get; }
+        double AirTemperature { get; }
+        double AirHumidity { get; }
+    }
+}
