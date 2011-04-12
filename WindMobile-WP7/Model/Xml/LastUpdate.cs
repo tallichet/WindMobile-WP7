1cf289667155fc6c75680e756fa97a01ed97e351
Cedric <ctt@veltigroup.com>
2011-04-12 11:33:13 +0200

    Add the Git Ignore
    Add job base

---
+++ WindMobile-WP7/Model/Xml/LastUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Ch.Epix.WindMobile.WP7.Model.Xml
+{
+    public class LastUpdate : ILastUpdate
+    {
+        private XElement _lastUpdate;
+
+        internal LastUpdate(XElement lastUpdate)
+        {
+            this._lastUpdate = lastUpdate;
+        }
+
+        public DateTime Date
+        {
+            get { return DateTime.Parse(_lastUpdate.Value); }
+        }
+    }
+}
---
+++ WindMobile-WP7/Service/Job/JobBase.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Ch.Epix.WindMobile.WP7.Service.Job
+{
+    public abstract class JobBase
+    {
+        private BackgroundWorker worker;
+        private WebClient client;
+
+        private string downloadedString;
+        private object result;
+
+        public JobBase()
+        {
+            InitWebClient();
+            InitWorker();
+        }
+
+        /// <summary>
+        /// Executed before doing anything else. 
+        /// Thread: UI
+        /// </summary>
+        protected virtual void Init() { }
+
+        /// <summary>
+        /// Just return the URL for the download string
+        /// </summary>
+        /// <returns>Url to access the data</returns>
+        protected virtual Uri GetUrl()
+        {
+            return new Uri("http://windmobile.vol-libre-suchet.ch:1588/windmobile/");
+        }
+
+        /// <summary>
+        /// Occurs when the string was downloaded correctly
+        /// </summary>
+        /// <param name="downloadedString">The resulting string</param>
+        /// <returns></returns>
+        protected virtual Object OnDownloadStringCompleted(String downloadedString) { return null; }
+        /// <summary>
+        /// Allow to process download error message
+        /// </summary>
+        /// <param name="exception">Error exception</param>
+        protected virtual void OnDownloadStringError(Exception exception) { }
+
+        /// <summary>
+        /// This happen in the UI Thread, just before starting the background worker
+        /// </summary>
+        /// <param name="arg">Object to use as argument for the background work</param>
+        protected virtual void OnJobStarting(object arg) { }
+
+        /// <summary>
+        /// Background work. Done in the Background Thread (no access to UI thread Dependency properties)
+        /// </summary>
+        /// <param name="cancel">Was the job canceled?</param>
+        /// <param name="arg">argument for the background job</param>
+        /// <returns>Object return by the </returns>
+        protected virtual Object JobRun(ref bool cancel, object arg) { return null; }
+
+        /// <summary>
+        /// Occurs after the job ends, in the UI Thread, just before the JobCompleted event raise
+        /// </summary>
+        /// <param name="result">Result of the background job</param>
+        protected virtual void OnJobCompleted(object result) { }
+
+        /// <summary>
+        /// Occurs when the job was completed
+        /// </summary>
+        public event EventHandler<JobFinishedEventArgs> JobCompleted;
+
+        /// <summary>
+        /// Start a job when needing to download
+        /// </summary>
+        public void StartDownloadJob()
+        {
+            Init();
+            if (client.IsBusy == false)
+            {
+                client.DownloadStringAsync(GetUrl());
+            }
+        }
+
+        /// <summary>
+        /// Start a background job without download
+        /// </summary>
+        /// <param name="arg">Argument for the background job</param>
+        public void StartBackgroundJob(object arg)
+        {
+            Init();
+            worker.RunWorkerAsync(arg);
+        }
+
+        private void InitWebClient() 
+        {
+            Init();
+            client = new WebClient();
+            client.DownloadStringCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                {
+                    OnDownloadStringError(e.Error);
+                }
+                else if (e.Cancelled == false)
+                {
+                    result = OnDownloadStringCompleted(e.Result);
+                }
+                worker.RunWorkerAsync(downloadedString);
+            };
+        }
+
+        private void InitWorker()
+        {
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += (s, e) =>
+                {
+                    bool cancel = false;
+                    e.Result = JobRun(ref cancel, e.Argument);
+                };
+            worker.RunWorkerCompleted += (s, e) =>
+                {
+                    if (e.Cancelled == false && e.Error == null)
+                    {
+                        result = e.Result;
+                        RaiseJobCompleted(result);
+                    }
+                };
+        }
+
+        private void RaiseJobCompleted(object result)
+        {
+            if (JobCompleted != null)
+            {
+                JobCompleted(this, new JobFinishedEventArgs(result));
+            }
+        }
+    }
+
+    public class JobFinishedEventArgs : EventArgs
+    {
+        internal JobFinishedEventArgs(object r)
+        {
+            Result = r;
+        }
+
+        public object Result { get; private set; }
+    }
+}
