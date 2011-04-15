c26f29368469e6a37c0589c03e3462f6941027c7
Cedric <cedric.tallichet@gmail.com>
2011-04-15 10:37:04 +0200

    Make it work for the listbox items

--- WindMobile-WP7/View/MainPanoramaView.xaml.cs
+++ WindMobile-WP7/View/MainPanoramaView.xaml.cs
@@ -10,11 +10,23 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Ch.Epix.WindMobile.WP7.Model;
+using System.Diagnostics;
 
 namespace Ch.Epix.WindMobile.WP7.View
 {
     public partial class MainPanoramaView : PhoneApplicationPage
     {
+        private ListBox lastUsedListBox = null;
+
+        public ViewModel.MainViewModel ViewModel
+        {
+            get
+            {
+                return (DataContext as ViewModel.MainViewModel);
+            }
+        }
+
         public MainPanoramaView()
         {
             InitializeComponent();
@@ -25,9 +37,22 @@ namespace Ch.Epix.WindMobile.WP7.View
             //NavigationService.Navigate(new Uri("/View/StationInfoView.xaml", UriKind.Relative));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/View/StationInfoView.xaml", UriKind.Relative));
+            // Are we selecting an item, and not desellecting one?
+            if (e.AddedItems.Count > 0)
+            {
+                lastUsedListBox = sender as ListBox; // keep track of the list box to clear it after navigated back
+                NavigationService.Navigate(new Uri("/View/StationInfoView.xaml", UriKind.Relative));
+            }
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (lastUsedListBox != null)
+            {
+                lastUsedListBox.SelectedItem = null;
+            }
         }
     }
 }
