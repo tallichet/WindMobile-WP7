f5d81574ec2c857680382caf80a0c6712ec232c0
Cedric <kridek@gmail.com>
2011-05-09 11:45:13 +0200

    Add settings page

---
+++ WindMobile-WP7/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Ch.Epyx.WindMobile.WP7.Model;
+using System.IO.IsolatedStorage;
+
+namespace Ch.Epyx.WindMobile.WP7.ViewModel
+{
+    public class SettingsViewModel : ApplicationViewModel
+    {
+        public static string UsernameStatic
+        {
+            get
+            {
+                string username;
+                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<string>("username", out username))
+                {
+                    return username;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                IsolatedStorageSettings.ApplicationSettings["username"] = value;
+            }
+        }
+
+        public static string PasswordStatic
+        {
+            get
+            {
+                string password;
+                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<string>("password", out password))
+                {
+                    return password;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                IsolatedStorageSettings.ApplicationSettings["password"] = value;
+            }
+        }
+
+        public static ICredentials Credentials
+        {
+            get { return new NetworkCredential(UsernameStatic, PasswordStatic); }
+        }
+
+        public string Username
+        {
+            get { return UsernameStatic; }
+            set { UsernameStatic = value; }
+        }
+
+        public string Password
+        {
+            get { return PasswordStatic; }
+            set { PasswordStatic = value; }
+        }
+
+        public string PageTitle
+        {
+            get { return "paramètres"; }
+        }
+        
+    }
+}
