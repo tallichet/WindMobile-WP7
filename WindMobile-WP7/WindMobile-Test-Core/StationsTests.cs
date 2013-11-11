a79d4be369f389cc74bb817b5eae4a22c981f4b2
Cédric Tallichet <kridek@gmail.com>
2013-11-11 23:39:31 +0100

    Adding tests project

---
+++ WindMobile-WP7/WindMobile-Test-Core/StationsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch.Epyx.WindMobile.Core.Test
+{
+    [TestClass]
+    public class StationsTests
+    {
+        [TestMethod]
+        public async void TestLists()
+        {
+            var networkService = new Core.Service.NetworkService();
+
+            var stations = await networkService.ListStations(10);
+
+            Assert.IsNotNull(stations, "no stations retuned");
+            Assert.AreEqual(10, stations.Count, "limit not respected");
+            Assert.AreEqual("Demo Endesa", stations[0].ShortName, "not the correct short name");
+        }
+    }
+}
